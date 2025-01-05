cff0315bd61f7dc9a5b2dceafb0fdd9948c41fcf
LouiIII3 <jikdsunghuilee@naver.com>
2025-01-05 14:57:20 +0900

    [setting/#0] eslint 컨벤션 구성

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,24 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {     
+    rules: {
+      // 들여쓰기 2칸
+      indent: ["error", 2],
+      // 세미콜론 항상 사용하기
+      semi: ["error", "always"],
+      // 카멜 케이스
+      camelcase: ["error", { properties: "always" }],
+      // var 사용 안함
+      "no-var": "error",
+      // 화살표 함수 사용 권장
+      "prefer-arrow-callback": "error",
+      // 템플릿 문자열 사용
+      "prefer-template": "error",
+       // 연산자 공백
+      "space-infix-ops": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
