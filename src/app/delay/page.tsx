23c4a1c74f7f9278a36e53dce008a7f62c0c26b5
LouiIII3 <jikdsunghuilee@naver.com>
2025-01-07 09:15:59 +0900

    [feat/#1] loading 페이지 구현

---
+++ src/app/delay/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import LoadingWrapper from '@/components/LoadingWrapper';
+
+const DelayedLoadingPage = () => {
+  return (
+    <div>
+      <LoadingWrapper />
+    </div>
+  );
+};
+
+export default DelayedLoadingPage;
