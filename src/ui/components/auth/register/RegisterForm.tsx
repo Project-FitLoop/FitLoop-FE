ca9fb559e2fdf53407cbb2b44c5a13b6613858e9
LouiIII3 <jikdsunghuilee@naver.com>
2025-01-25 11:48:42 +0900

    [feat/#4] 회원가입 단계 추가

--- src/ui/components/auth/register/RegisterForm.tsx
+++ src/ui/components/auth/register/RegisterForm.tsx
@@ -5,6 +5,13 @@ import { Typography, Progress, Button } from "antd";
 
 const { Title } = Typography;
 
+const steps = [
+  { title: "아이디를\n입력해 주세요.", field: "username", placeholder: "아이디" },
+  { title: "비밀번호를\n입력해 주세요.", field: "password", placeholder: "비밀번호" },
+  { title: "이메일을\n입력해 주세요.", field: "email", placeholder: "이메일" },
+  { title: "이름을\n입력해 주세요.", field: "name", placeholder: "이름" },
+];
+
 const RegisterForm = () => {
   return (
     <div
@@ -22,7 +29,7 @@ const RegisterForm = () => {
       {/* 상단 진행 상황 */}
       <div style={{ padding: "20px 16px" }}>
         <Progress percent={25} showInfo={false} />
-        <Title level={5}>아이디를 입력해 주세요.</Title>
+        <Title level={5}>{steps[0].title}</Title>
       </div>
 
       {/* 하단 버튼 */}
