c3bfed54e070b43853095c61d766deed8d89a12f
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-04 17:44:58 +0900

    [feat/$] 포트번호 .env 파일로 변경

--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
 // 로그인 API 호출
 export const loginUser = async (username: string, password: string): Promise<string> => {
