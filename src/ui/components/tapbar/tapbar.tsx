6a50a6929588f9bc7d9f32609eb53dc7a38a7408
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-06 20:06:28 +0900

    [feat/#6] tab bar 구현

--- src/ui/components/tapbar/tapbar.tsx
+++ src/ui/components/tapbar/tapbar.tsx
@@ -3,15 +3,16 @@
 import React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import Image from "next/image";
 import { Menu } from "antd";
-import styles from "@/ui/components/tapbar/tab-bar.module.css"; // CSS 파일 불러오기
+import styles from "@/ui/components/tapbar/tab-bar.module.css";
 
 const tabs = [
-    { name: "메인", path: "/", icon: "/assets/main.png" },
-    { name: "룩앤폼", path: "/look", icon: "/assets/look.png" },
-    { name: "마이샵", path: "/myshop", icon: "/assets/myshop.png", special: true },
-    { name: "커뮤니티", path: "/community", icon: "/assets/community.png" },
-    { name: "마이페이지", path: "/mypage", icon: "/assets/mypage.png" },
+  { name: "메인", path: "/", icon: "/assets/main.png" },
+  { name: "룩앤폼", path: "/look", icon: "/assets/look.png" },
+  { name: "마이샵", path: "/myshop", icon: "/assets/myshop.png", special: true },
+  { name: "커뮤니티", path: "/community", icon: "/assets/community.png" },
+  { name: "마이페이지", path: "/mypage", icon: "/assets/mypage.png" },
 ];
 
 const hiddenPages = ["/admin"];
@@ -25,7 +26,7 @@ const TabBar: React.FC = () => {
     label: (
       <Link href={tab.path} className={`${styles.tabItem} ${tab.special ? styles.specialTab : ""}`}>
         <div className={`${styles.iconWrapper} ${tab.special ? styles.specialIconWrapper : ""}`}>
-          <img src={tab.icon} alt={tab.name} width="24" height="24" className="mb-1" />
+          <Image src={tab.icon} alt={tab.name} width={24} height={24} priority />
         </div>
         <span className={`${styles.tabText} ${tab.special ? styles.specialText : ""}`}>{tab.name}</span>
       </Link>
@@ -34,12 +35,7 @@ const TabBar: React.FC = () => {
 
   return (
     <div className={styles.tabBar}>
-      <Menu
-        mode="horizontal"
-        selectedKeys={[pathname]}
-        className={styles.menu}
-        items={menuItems}
-      />
+      <Menu mode="horizontal" selectedKeys={[pathname]} className={styles.menu} items={menuItems} />
     </div>
   );
 };
