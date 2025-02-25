143f36ae5d71c9238bc8a6569548a790ae9e0a4c
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-25 23:05:20 +0900

    [feat/#16] Settings - 로그아웃 설정 페이지 추가

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Settings from "@/ui/components/setting/setting"; // ✅ settings.tsx에서 UI 분리
+
+const SettingsPage: React.FC = () => {
+  return <Settings />;
+};
+
+export default SettingsPage;
