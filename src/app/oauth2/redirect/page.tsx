032aa8d72c6fdb8c76b83f9672da8057a2be2b7c
LouiIII3 <jikdsunghuilee@naver.com>
2025-02-28 12:29:35 +0900

    [feat/#16] redirect - 중복 체크 로직 추가

--- src/app/oauth2/redirect/page.tsx
+++ src/app/oauth2/redirect/page.tsx
@@ -1,39 +1,31 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useRouter } from "next/navigation";
 import { message } from "antd";
 import { reissueAccessToken } from "@/services/api/auth";
 
 const OAuthRedirect = () => {
   const router = useRouter();
+  const isProcessing = useRef(false);
 
   useEffect(() => {
-    // 이미 처리되었다면 실행하지 않음
-    if (sessionStorage.getItem("reissueProcessed")) return;
+    if (isProcessing.current) return;
+    isProcessing.current = true;
 
     const fetchAccessToken = async () => {
       try {
-        // Access Token 요청
+        console.log("Access Token 요청 시작");
         const accessToken = await reissueAccessToken();
 
-        // Access Token을 로컬 스토리지에 저장
         window.localStorage.setItem("access", accessToken);
         message.success("OAuth2 로그인 성공!");
 
-        // 로그인 성공 시에만 플래그 설정
-        sessionStorage.setItem("reissueProcessed", "true");
-
-        // 로그인 성공 후 대시보드 페이지로 이동
         router.push("/dashboard");
       } catch (error: unknown) {
         console.error("Access Token 요청 실패:", error);
         message.error("OAuth2 로그인 실패!");
 
-        // 로그인 실패 시 플래그 제거
-        sessionStorage.removeItem("reissueProcessed");
-
-        // 로그인 페이지로 리다이렉트
         router.push("/login");
       }
     };
