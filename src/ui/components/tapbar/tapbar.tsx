bfbd344fa245111568b96472e92af0335b85b4e4
LouiIII3 <jikdsunghuilee@naver.com>
2025-03-01 12:57:30 +0900

    [style/#18] png 파일 svg 파일로 변경

--- src/ui/components/tapbar/tapbar.tsx
+++ src/ui/components/tapbar/tapbar.tsx
@@ -7,11 +7,11 @@ import Image from "next/image";
 import styles from "@/ui/components/tapbar/tab-bar.module.css";
 
 const tabs = [
-  { name: "메인", path: "/", icon: "/assets/main.png" },
-  { name: "룩앤폼", path: "/look", icon: "/assets/look.png" },
-  { name: "마이샵", path: "/myshop", icon: "/assets/myshop.png", special: true },
-  { name: "커뮤니티", path: "/community", icon: "/assets/community.png" },
-  { name: "마이페이지", path: "/mypage", icon: "/assets/mypage.png" },
+  { name: "메인", path: "/", icon: "/assets/main.svg" },
+  { name: "룩앤폼", path: "/look", icon: "/assets/look.svg" },
+  { name: "마이샵", path: "/myshop", icon: "/assets/myshop.svg", special: true },
+  { name: "커뮤니티", path: "/community", icon: "/assets/community.svg" },
+  { name: "마이페이지", path: "/mypage", icon: "/assets/mypage.svg" },
 ];
 
 const hiddenPages = ["/admin", "/register"];
@@ -24,11 +24,26 @@ const TabBar: React.FC = () => {
     <div className={styles.tabBar}>
       <div className={styles.menu}>
         {tabs.map((tab) => (
-          <Link key={tab.path} href={tab.path} className={`${styles.tabItem} ${tab.special ? styles.specialTab : ""}`}>
-            <div className={`${styles.iconWrapper} ${tab.special ? styles.specialIconWrapper : ""}`}>
-              <Image src={tab.icon} alt={tab.name} width={24} height={24} priority />
+          <Link
+            key={tab.path}
+            href={tab.path}
+            className={`${styles.tabItem} ${tab.special ? styles.specialTab : ""}`}
+          >
+            <div
+              className={`${styles.iconWrapper} ${tab.special ? styles.specialIconWrapper : ""}`}
+            >
+              <Image
+                src={tab.icon}
+                alt={tab.name}
+                width={24}
+                height={24}
+                priority
+                className={styles.icon}
+              />
             </div>
-            <span className={`${styles.tabText} ${tab.special ? styles.specialText : ""}`}>{tab.name}</span>
+            <span className={`${styles.tabText} ${tab.special ? styles.specialText : ""}`}>
+              {tab.name}
+            </span>
           </Link>
         ))}
       </div>
