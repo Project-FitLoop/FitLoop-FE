1d8ac05d9f0c9edec756e420a00c2079982e758e
LouiIII3 <jikdsunghuilee@naver.com>
2025-03-04 23:17:05 +0900

    [feat/#20] 헤더에 있는 뒤로가기 버튼 수정(전 페이지로 돌아감)

--- src/ui/components/header/Header.tsx
+++ src/ui/components/header/Header.tsx
@@ -1,14 +1,27 @@
-import Link from "next/link";
+"use client";
+
+import { useRouter } from "next/navigation";
 import Image from "next/image";
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <div className="flex items-center justify-between px-4 py-3 bg-white shadow-md">
       {/* 뒤로 가기 버튼 */}
-      <Link href="/" className="flex items-center text-lg font-bold text-gray-800">
+      <button
+        onClick={() => {
+          if (window.history.length > 1) {
+            router.back();
+          } else {
+            router.push("/");
+          }
+        }}
+        className="flex items-center text-lg font-bold text-gray-800"
+      >
         <Image src="/assets/product-tab/left-arrow.svg" alt="뒤로 가기" width={20} height={20} className="mr-2" />
         상품
-      </Link>
+      </button>
 
       <div className="flex space-x-4">
         <button>
