aa466564d2cc09e7a8935f77567fe5ff79b58b19
Jieun-KWAK <jieun47631@gmail.com>
2025-03-14 21:29:12 +0900

    [feat/#21] user.ts에 프로필 추가 API 작성

--- src/services/api/user.ts
+++ src/services/api/user.ts
@@ -16,3 +16,27 @@ export const fetchUserInfo = async (): Promise<{ username: string }> => {
 
   return response.data; 
 };
+
+//프로필 추가 API
+export const registerInfomation = async (
+  nickname: string,
+  gender: string,
+  ageRange: string,
+  height: number,
+  weight: number
+): Promise<boolean> => {
+  try {
+    const response = await instance.post("/users/profile", {
+      nickname,
+      gender,
+      ageRange,
+      height,
+      weight,
+    });
+
+    return response.status === 201; // 성공 시 true 반환
+  } catch (error) {
+    console.error("프로필 등록 실패:", error);
+    return false; // 실패 시 false 반환
+  }
+};
