199a088189944eec460be6f554ae7372ce9ecc60
Jieun-KWAK <jieun47631@gmail.com>
2025-03-27 15:01:46 +0900

    [feat/#23] uploadImages 인스턴스 수정

--- src/services/api/imageUpload.ts
+++ src/services/api/imageUpload.ts
@@ -2,15 +2,13 @@ import { instance } from "@/config/apiConfig";
 
 export const uploadImages = async (files: File[]): Promise<string[]> => {
   const formData = new FormData();
-  files.forEach((file) => formData.append("images", file));
+  files.forEach((file) => formData.append("file", file));
 
   try {
-    const response = await instance.post("/productregister/image", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
+    const response = await instance.post("/upload", formData);
 
+    console.log(response.data.imageUrls);
+    
     return response.data.imageUrls;
   } catch (error) {
     console.error("이미지 업로드 오류:", error);
