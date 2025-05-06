b411e67819836f261c71f8ca18092e9d5786a3e0
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-06 20:17:15 +0900

    [feat/#37] 상품 상세 페이지 라우팅을 /products/[id]로 변경

---
+++ src/app/products/[id]/page.tsx
@@ -0,0 +1,32 @@
+export const dynamicParams = true;
+
+import ProductDetailPage from '@/ui/components/product/ProductDetailPage';
+import { fetchProductDetail } from '@/services/api/productApi';
+
+interface PageProps {
+  params: { id: string };
+}
+
+export default async function ProductDetail({ params }: PageProps) {
+  const { id } = params;
+  const product = await fetchProductDetail(id);
+
+  return (
+    <div
+      id="scrollable-container"
+      className="scrollbar-hide"
+      style={{
+        height: '100vh',
+        overflowY: 'auto',
+        display: 'flex',
+        flexDirection: 'column',
+        scrollBehavior: 'smooth',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ width: '100%', maxWidth: '400px' }}>
+        <ProductDetailPage product={product} />
+      </div>
+    </div>
+  );
+}
