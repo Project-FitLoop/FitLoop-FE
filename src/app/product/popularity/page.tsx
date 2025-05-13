546e8ace69f5ace2a3a3a093fb4139c2e00c2591
Jieun-KWAK <jieun47631@gmail.com>
2025-05-13 19:43:28 +0900

    [feat/#29] 인기 콘텐츠 페이지 구성 및 FloatingActionButton 적용

--- src/app/product/popularity/page.tsx
+++ src/app/product/popularity/page.tsx
@@ -1,4 +1,19 @@
+import Popular from '@/ui/components/popularity/PopularityPage';
+import FloatingActionButton from '@/ui/components/common/FloatingActionButton';
+
 export default function PopularityPage() {
-  return <div>인기 상품 페이지</div>;
+  return (
+    <div
+      id="scrollable-container"
+      style={{
+        height: '100vh',
+        overflowY: 'auto',
+        scrollBehavior: 'smooth',
+      }
+      } className="scrollbar-hide"
+    >
+      <Popular />
+      <FloatingActionButton />
+    </div>
+  );
 }
-  
