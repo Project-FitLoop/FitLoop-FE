53ffcb45e687b7fe4add765b5f4038595f311644
LouiIII3 <jikdsunghuilee@naver.com>
2025-05-15 00:07:11 +0900

    [feat/#42] fadeInLogo 및 fadeInUp 애니메이션 설정 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,20 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      keyframes: {
+        fadeInLogo: {
+          '0%': { opacity: '0', transform: 'translateY(-20px)' },
+          '100%': { opacity: '1', transform: 'translateY(0)' },
+        },
+        fadeInUp: {
+          '0%': { opacity: '0', transform: 'translateY(40px)' },
+          '100%': { opacity: '1', transform: 'translateY(-12%)' },
+        },
+      },
+      animation: {
+        fadeInLogo: 'fadeInLogo 1s ease-out 0.3s forwards',
+        fadeInUp: 'fadeInUp 0.9s ease-out 0.5s forwards',
+      },
     },
   },
   plugins: [],
