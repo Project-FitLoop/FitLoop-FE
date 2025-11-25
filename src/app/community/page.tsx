c94c04c73b923c6bf3099af3a32953e08e9e6f88
LouiIII3 <jikdsunghuilee@naver.com>
2025-11-25 18:10:28 +0900

    [feat/#57] community page 기본 페이지 구성

---
+++ src/app/community/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import CommunityPage from "@/ui/components/community/CommunityPage";
+
+export default function CommunityRoutePage() {
+  return (
+    <div
+      style={{
+        maxWidth: "393px",
+        margin: "0 auto",
+        width: "100%",
+        height: "100vh",
+        overflowY: "auto",
+      }}
+    >
+      <CommunityPage />
+    </div>
+  );
+}
