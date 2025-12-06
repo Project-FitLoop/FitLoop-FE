37fc385493e5d5be0aa73697348b98d86ce13c3a
LouiIII3 <jikdsunghuilee@naver.com>
2025-12-06 14:13:27 +0900

    [feat/#57] 탭바 수정 및 상품등록 페이지 레이아웃 크기 조정

--- src/app/products/register/page.tsx
+++ src/app/products/register/page.tsx
@@ -1,9 +1,23 @@
+"use client";
+
 import ProductRegisterForm from "@/ui/components/product/ProductRegisterForm";
 
 const ProductRegisterPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center overflow-auto">
-      <div className="w-full max-w-[400px]">
+    <div
+      id="scrollable-container"
+      className="scrollbar-hide"
+      style={{
+        height: "100vh",
+        overflowY: "auto",
+        scrollBehavior: "smooth",
+      }}
+    >
+      {/* 가운데 width 400px 고정 영역 */}
+      <div
+        style={{ width: "100%", maxWidth: "400px" }}
+        className="mx-auto relative px-4 pb-10"
+      >
         <ProductRegisterForm />
       </div>
     </div>
